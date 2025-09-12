11adcc62ba968bad457dc826f44713be46a114f7
Roman Bukin <roman@vanbukin.com>
2025-09-12 18:34:12 +0300

    Add initial project structure with configuration files and solution setup

---
+++ src/ContextLeech/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ContextLeech;
+
+[SuppressMessage("Maintainability", "CA1515:Consider making public types internal")]
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
